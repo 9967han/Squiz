efeefff05cb7e1e5a4435634d8c1be8ffb4e448e
SangWook <9967han@naver.com>
2018-04-30 16:21:24 +0900

    최초커밋

---
+++ app/src/main/java/swp3/skku/edu/squiz/MainActivity.java
@@ -0,0 +1,13 @@
+package swp3.skku.edu.squiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
