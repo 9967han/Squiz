b8152b774e09cd81337d75d2008b02938f49c701
ppuzzle <--wldn0202@gmail.com>
2018-05-09 11:38:03 +0900

    Add Hamburger menu and DrawerLayout

--- app/src/main/java/swp3/skku/edu/squiz/MainActivity.java
+++ app/src/main/java/swp3/skku/edu/squiz/MainActivity.java
@@ -1,9 +1,12 @@
 package swp3.skku.edu.squiz;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,15 +15,19 @@ import android.widget.Toast;
 
 import swp3.skku.edu.squiz.MakeCard.MakeCardActivity;
 
+
 public class MainActivity extends AppCompatActivity {
-
-
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
 
         viewPager = findViewById(R.id.main_view_pager);
         TabLayout tabLayout = findViewById(R.id.main_tab_layout);
@@ -43,5 +50,9 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, MakeCardActivity.class);
             startActivity(intent);
         }
+    }
+    public void onOptionItemSelected(View view) {
+        drawerLayout.openDrawer(GravityCompat.START);
+
     }
 }
