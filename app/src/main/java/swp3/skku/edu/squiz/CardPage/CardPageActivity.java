8f4988bcf7bf156d9067d32324835143c9c190ac
SangWook <9967han@naver.com>
2018-05-15 22:43:00 +0900

    큰일났다 커밋오류뜨나? 스테이지에 안올리고 커밋해버림 ㅁㄴㅇㄹ

--- app/src/main/java/swp3/skku/edu/squiz/CardPage/CardPageActivity.java
+++ app/src/main/java/swp3/skku/edu/squiz/CardPage/CardPageActivity.java
@@ -5,21 +5,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 
 import swp3.skku.edu.squiz.R;
+import swp3.skku.edu.squiz.WordCardPage.WordCardActivity;
 
 /**
  * Created by LG on 2018-05-12.
  */
 
-public class CardPageActivity extends AppCompatActivity {
+public class CardPageActivity extends AppCompatActivity implements View.OnClickListener{
     TextView cardpage_title;
     TextView cardpage_count;
     TextView cardpage_like_count;
     Adapter_cardPage adapter_cardPage;
     RecyclerView cardpage_RV;
-
+    TextView cardpage_word_card;
+    TextView cardpage_subjective;
+    TextView cardpage_learning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +32,46 @@ public class CardPageActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String title = intent.getStringExtra("title");
         String count = intent.getStringExtra("count");
-        cardpage_title = findViewById(R.id.cardpage_title);
-        cardpage_count = findViewById(R.id.cardpage_count);
-        cardpage_like_count = findViewById(R.id.cardpage__like_count);
-        cardpage_RV = findViewById(R.id.cardpage_RV);
+
+        findViews();
+
         cardpage_title.setText("[\t\t\t"+title+"\t\t\t]");
         cardpage_count.setText("총 "+ count+"단어");
+        cardpage_word_card.setOnClickListener(this);
+        cardpage_subjective.setOnClickListener(this);
+        cardpage_learning.setOnClickListener(this);
 
         adapter_cardPage = new Adapter_cardPage(R.layout.cardpage_content, this, cardpage_like_count);
         adapter_cardPage.loadItemData(title);
         cardpage_RV.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         cardpage_RV.setAdapter(adapter_cardPage);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.cardpage_word_card:
+                Intent intent = new Intent(CardPageActivity.this, WordCardActivity.class);
+                intent.putExtra("list", adapter_cardPage.getCardPageItemList());
+                startActivity(intent);
+                break;
+            case R.id.cardpage_subjective:
+                break;
+            case R.id.cardpage_learning:
+                break;
+        }
+    }
+
+    private void findViews(){
+        cardpage_title = findViewById(R.id.cardpage_title);
+        cardpage_count = findViewById(R.id.cardpage_count);
+        cardpage_like_count = findViewById(R.id.cardpage__like_count);
+        cardpage_RV = findViewById(R.id.cardpage_RV);
+        cardpage_word_card = findViewById(R.id.cardpage_word_card);
+        cardpage_subjective = findViewById(R.id.cardpage_subjective);
+        cardpage_learning = findViewById(R.id.cardpage_learning);
+    }
+
+
+
 }
--- app/src/main/java/swp3/skku/edu/squiz/WordCardPage/WordCardActivity.java
+++ app/src/main/java/swp3/skku/edu/squiz/WordCardPage/WordCardActivity.java
@@ -1,15 +1,38 @@
-package swp3.skku.edu.squiz.CardPage;
+package swp3.skku.edu.squiz.WordCardPage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import swp3.skku.edu.squiz.R;
+import swp3.skku.edu.squiz.model.CardItem;
 
 public class WordCardActivity extends AppCompatActivity {
+
+    private RecyclerView wordCardRV;
+    private ArrayList<CardItem> cardPageItemList = new ArrayList<>();
+    Adapter_wordCard adapter_wordCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_card);
-
+        Intent intent = getIntent();
+        cardPageItemList = (ArrayList<CardItem>) intent.getSerializableExtra("list");
+        adapter_wordCard = new Adapter_wordCard(R.layout.word_card_content, this, cardPageItemList, getResources());
+        findViews();
+        wordCardRV.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        wordCardRV.setAdapter(adapter_wordCard);
     }
+
+
+    private void findViews() {
+        wordCardRV = findViewById(R.id.wordCardRV);
+    }
+
+
 }
