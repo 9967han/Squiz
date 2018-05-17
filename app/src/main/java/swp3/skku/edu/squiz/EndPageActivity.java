0fdbc11643a7310f677bdf180d2c9b96cbfe0586
SangWook <9967han@naver.com>
2018-05-18 01:56:58 +0900

    하은이가한거 구경해야지

---
+++ app/src/main/java/swp3/skku/edu/squiz/EndPageActivity.java
@@ -0,0 +1,13 @@
+package swp3.skku.edu.squiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EndPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);;
+        setContentView(R.layout.endpage);
+    }
+}
--- app/src/main/java/swp3/skku/edu/squiz/SubjectiveCardPage/ViewHolder_subjective.java
+++ app/src/main/java/swp3/skku/edu/squiz/SubjectiveCardPage/ViewHolder_subjective.java
@@ -3,6 +3,7 @@ package swp3.skku.edu.squiz.SubjectiveCardPage;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import swp3.skku.edu.squiz.R;
@@ -14,12 +15,14 @@ import swp3.skku.edu.squiz.R;
 public class ViewHolder_subjective extends RecyclerView.ViewHolder {
     public TextView question;
     public EditText meaning;
+    public ImageView correct;
     int position;
 
     public ViewHolder_subjective(View itemView) {
         super(itemView);
         question = itemView.findViewById(R.id.subjective_question);
         meaning = itemView.findViewById(R.id.subjective_answer);
+        correct = itemView.findViewById(R.id.correct_circle);
     }
 }
 
