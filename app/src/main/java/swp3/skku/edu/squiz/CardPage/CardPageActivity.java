45c112b64dd01a0f6adeeb5d04f2c4eab87762fc
haeun kim <khe49260@naver.com>
2018-05-31 10:54:20 +0900

    카드 수정 후 빈 페이지 수정

--- app/src/main/java/swp3/skku/edu/squiz/CardPage/CardPageActivity.java
+++ app/src/main/java/swp3/skku/edu/squiz/CardPage/CardPageActivity.java
@@ -97,7 +97,6 @@ public class CardPageActivity extends AppCompatActivity implements View.OnClickL
                 //return true;
             }
             case R.id.edit_card:{
-                setContentView(R.layout.edit_cardpage);
                 Toast.makeText(getApplicationContext(), "카드수정하기", Toast.LENGTH_SHORT).show();
                 Intent editIntent = new Intent(CardPageActivity.this, EditCardActivity.class);
                 editIntent.putExtra("title", title);
