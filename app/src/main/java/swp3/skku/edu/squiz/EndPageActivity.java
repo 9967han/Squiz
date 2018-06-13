861ba6a8131fac48c0b5920a6193d3ce4d60cb32
ppuzzle <--wldn0202@gmail.com>
2018-06-14 02:48:44 +0900

    주관식 완료하고 다시 시작하기, 낱말카드 다시보기 잘 안눌려지는 버그 수정 낱말카드 다시보기 누르면 카드 페이지 뜨게 수정

--- app/src/main/java/swp3/skku/edu/squiz/EndPageActivity.java
+++ app/src/main/java/swp3/skku/edu/squiz/EndPageActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,6 +19,8 @@ public class EndPageActivity extends AppCompatActivity {
 
     private TextView restartSubjective;
     private TextView restartCardPage;
+    private LinearLayout end_replay;
+    private LinearLayout end_star;
     String title;
 
 
@@ -27,6 +30,8 @@ public class EndPageActivity extends AppCompatActivity {
         setContentView(R.layout.endpage);
         restartSubjective = findViewById(R.id.restartSub);
         restartCardPage = findViewById(R.id.restartCardPage);
+        end_replay = findViewById(R.id.end_replay);
+        end_star = findViewById(R.id.end_star);
 
         restartSubjective.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,9 +47,33 @@ public class EndPageActivity extends AppCompatActivity {
         restartCardPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentb = new Intent(getApplicationContext(), WordCardActivity.class);
+                Intent intentb = new Intent(getApplicationContext(), CardPageActivity.class);
                 Intent tempb = getIntent();
-                intentb.putExtra("list", tempb.getSerializableExtra("cardSet"));
+                intentb.putExtra("title", tempb.getStringExtra("title"));
+                intentb.putExtra("count", String.valueOf(((ArrayList<CardItem>)tempb.getSerializableExtra("cardSet")).size()));
+                startActivity(intentb);
+
+            }
+        });
+
+        end_replay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intenta = new Intent(getApplicationContext(), SubjectiveCardActivity.class);
+                Intent tempa = getIntent();
+                intenta.putExtra("title", tempa.getStringExtra("title"));
+                intenta.putExtra("list", tempa.getSerializableExtra("cardSet"));
+                startActivity(intenta);
+            }
+        });
+
+        end_star.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentb = new Intent(getApplicationContext(), CardPageActivity.class);
+                Intent tempb = getIntent();
+                intentb.putExtra("title", tempb.getStringExtra("title"));
+                intentb.putExtra("count", String.valueOf(((ArrayList<CardItem>)tempb.getSerializableExtra("cardSet")).size()));
                 startActivity(intentb);
 
             }
